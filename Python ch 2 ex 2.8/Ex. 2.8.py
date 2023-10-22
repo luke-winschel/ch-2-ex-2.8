8feb4a19a6b67ec24b24d4b65717bd2efc5c6aa0
luke-winschel <121312507+luke-winschel@users.noreply.github.com>
2023-10-22 17:22:14 -0500

    Add files via upload

---
+++ Python ch 2 ex 2.8/Ex. 2.8.py
@@ -0,0 +1,11 @@
+#Exercise 2.8
+""" (Table of Squares and cubes)  Write a script that calculates tghe squares and cubes of the numbers 0 to 5.  Print the resulting values in table format."""
+
+# Create headers using print statement
+print ('number\tsquare\tcube')
+print (0,'\t',0**2,'\t',0**3)
+print (1,'\t',1**2,'\t',1**3)
+print (2,'\t',2**2,'\t',2**3)
+print (3,'\t',3**2,'\t',3**3)
+print (4,'\t',4**2,'\t',4**3)
+print (5,'\t',5**2,'\t',5**3)
